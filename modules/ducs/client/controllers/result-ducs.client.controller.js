986cad414304cfd6c2a4397f6d9f0ceab6145645
tasellos <31318233+tasellos@users.noreply.github.com>
2017-10-12 15:06:13 -0400

    Update Parameters for DU categories in controller
    Kati updated the parameters for categorization

--- modules/ducs/client/controllers/result-ducs.client.controller.js
+++ modules/ducs/client/controllers/result-ducs.client.controller.js
@@ -18,12 +18,20 @@
               .then(function(response) {
                 $scope.ducs = response.data;
 
-                if($scope.ducs.results.uniformity_distribution > 0.9)
+                if($scope.ducs.results.uniformity_distribution > 0.84)
+                	$scope.result = "Exceptional";
+                else if($scope.ducs.results.uniformity_distribution >= 0.75 && $scope.ducs.results.uniformity_distribution <= 0.84)
                 	$scope.result = "Excellent";
-                else if($scope.ducs.results.uniformity_distribution >= 0.70 && $scope.ducs.results.uniformity_distribution <= 0.90)
-                	$scope.result = "Great";
+                else if($scope.ducs.results.uniformity_distribution >= 0.70 && $scope.ducs.results.uniformity_distribution <= 0.74)
+                	$scope.result = "Very Good";
+                else if($scope.ducs.results.uniformity_distribution >= 0.60 && $scope.ducs.results.uniformity_distribution <= 0.69)
+                	$scope.result = "Good";
+                else if($scope.ducs.results.uniformity_distribution >= 0.5 && $scope.ducs.results.uniformity_distribution <= 0.59)
+                	$scope.result = "Fair";
+                else if($scope.ducs.results.uniformity_distribution >= 0.4 && $scope.ducs.results.uniformity_distribution <= 0.49)
+                	$scope.result = "Poor";
                 else
-                	$scope.result = "Historical";
+                	$scope.result = "Fail";
 
               }, function(error) {  
                 $scope.error = 'Unable to retrieve listing with id "' + id + '"\n' + error;
