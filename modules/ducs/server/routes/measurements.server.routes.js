a4925f5edd58c8cfff73b708ea7d612c70c8b3e4
ychen259 <ychen259@ufl.edu>
2017-10-13 17:14:40 -0400

    Email result to myself

--- modules/ducs/server/routes/measurements.server.routes.js
+++ modules/ducs/server/routes/measurements.server.routes.js
@@ -19,6 +19,9 @@ module.exports = function(app) {
       .get(measurements.view)
       .delete(measurements.delete);
 
+    app.route('/api/email-result/:measureId')
+      .post(measurements.email);
+
     // Finish by binding the Duc middleware
     app.param('measureId', measurements.measurementByID);
 };
@@ -26,13 +29,10 @@ module.exports = function(app) {
 /*
 The 'router.param' method allows us to specify middleware we would like to use to handle
 requests with a parameter.
-
 Say we make an example request to '/listings/566372f4d11de3498e2941c9'
-
 The request handler will first find the specific listing using this 'listingsById'
 middleware function by doing a lookup to ID '566372f4d11de3498e2941c9' in the Mongo database,
 and bind this listing to the request object.
-
 It will then pass control to the routing function specified above, where it will either
 get, update, or delete that specific listing (depending on the HTTP verb specified)
 */
