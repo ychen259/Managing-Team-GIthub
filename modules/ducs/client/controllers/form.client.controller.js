b70f1a36e2f58009a4ae7df3c7236632e2c48b3c
ychen259 <ychen259@ufl.edu>
2017-10-12 21:34:58 -0400

    add unit conversion

--- modules/ducs/client/controllers/form.client.controller.js
+++ modules/ducs/client/controllers/form.client.controller.js
@@ -15,6 +15,7 @@
 
     $scope.can_depth = [];
     $scope.hide = true;
+    $scope.unit = false; //true for metric, false for English
     $scope.continue = function (isValid){
 
       if (!isValid) {
@@ -27,8 +28,10 @@
       var i;
       for (i=0;i < $scope.num ;i++)
       {
-        document.getElementById("can_depths").innerHTML += "<label>Amount of Water</label><input name = 'depth' id =" +
-        i + " type='number' required/><br>";
+        //document.getElementById("can_depths").innerHTML += "<div class='form-group'>";
+        document.getElementById("can_depths").innerHTML += "<label class='control-label'>Amount of Water</label><br>";
+        document.getElementById("can_depths").innerHTML += "<input name = 'depth' id =" + i + " type='number' class='form-control' required/><br>";
+       // document.getElementById("can_depths").innerHTML += "</div>"
       }
        /*document.getElementById("can_depths").innerHTML += "<div class='form-group'>" + 
                                                           "<button type='submit' class='btn btn-default' > submit</button>" +
@@ -48,10 +51,21 @@
         $scope.can_depth[i] = document.getElementById(i).value;
       }
 
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'articleForm');
+      /*Unit Conversion*/
+      /*unit == false (English -- inch);  unit == true (Metric -- mm)*/
+      /*1 inch = 25.4 mm*/
+      if($scope.unit == false){
+       console.log("English");
 
-        return false;
+       /*transfer inch to mm*/
+        for (i=0;i < $scope.num ;i++){
+          $scope.can_depth[i] = $scope.can_depth[i] * 25.4;
+        }
+       
+      }
+      else{ 
+        /*do nothing, because she wants metric unit in database*/
+        console.log("Metric");
       }
 
       /* Create the listing object */
