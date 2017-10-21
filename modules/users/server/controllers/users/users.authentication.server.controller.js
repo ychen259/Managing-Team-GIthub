7950df537bbdeba12693aa499393bf618567c577
carlosA-A <carlosA-A@users.noreply.github.com>
2017-10-21 09:38:58 -0400

    Send usename email instead of display name

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -25,7 +25,7 @@ exports.signup = function (req, res) {
   // Init user and add missing fields
   var user = new User(req.body);
   user.provider = 'local';
-  user.displayName = user.firstName + ' ' + user.lastName;
+  user.displayName = user.username;
 
   // Then save the user
   user.save(function (err) {
@@ -162,8 +162,6 @@ exports.saveOAuthUserProfile = function (req, providerUserProfile, done) {
 
         User.findUniqueUsername(possibleUsername, null, function (availableUsername) {
           user = new User({
-            firstName: providerUserProfile.firstName,
-            lastName: providerUserProfile.lastName,
             username: availableUsername,
             displayName: providerUserProfile.displayName,
             profileImageURL: providerUserProfile.profileImageURL,
