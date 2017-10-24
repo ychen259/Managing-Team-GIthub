3464d95c1b8f6c056fcf2a805c9be2a2e701c656
ychen259 <ychen259@ufl.edu>
2017-10-24 09:47:51 -0400

    better version of zipcode to county

--- modules/ducs/server/routes/measurements.server.routes.js
+++ modules/ducs/server/routes/measurements.server.routes.js
@@ -13,7 +13,7 @@ module.exports = function(app) {
     // Ducs Routes
     app.route('/api/measurements')
       .get(measurements.list)
-      .post(measurements.county,measurements.create);
+      .post(measurements.county, measurements.city, measurements.create);
     
       
 
