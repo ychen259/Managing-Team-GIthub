08a49e2ae4cf788291b507779ed9b30d74081af8
carlosA-A <carlosA-A@users.noreply.github.com>
2017-11-18 17:23:42 -0500

    added new input for counties

--- modules/ducs/client/controllers/form.client.controller.js
+++ modules/ducs/client/controllers/form.client.controller.js
@@ -20,6 +20,9 @@
     $scope.unit = false; //true for metric (cm), false for imperical (inch)
     $scope.volume = false; //true for volume, false for depth
 
+    //Store counties available
+    $scope.counties = ["Alachua","Baker","Bay","Bradford","Brevard","Broward","Calhoun","Charlotte","Citrus","Clay","Collier","Columbia","DeSoto","Dixie","Duval","Escambia","Flagler","Franklin","Gadsden","Gilchrist","Glades","Gulf","Hamilton","Hardee","Hendry","Hernando","Highlands","Hillsborough","Holmes","Indian River","Jackson","Jefferson","Lafayette","Lake","Lee","Leon","Levy","Liberty","Madison","Manatee","Marion","Martin","Miami-Dade","Monroe","Nassau","Okaloosa","Okeechobee","Orange","Osceola","Palm Beach","Pasco","Pinellas","Polk","Putnam","St. Johns","St. Lucie","Santa Rosa","Sarasota","Seminole","Sumter","Suwannee","Taylor","Union","Volusia","Wakulla","Walton","Washington"];
+
     $scope.idArrayForDepth = [];
     $scope.idArrayForVolume = [];
 
